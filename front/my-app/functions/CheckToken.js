b10b67c64f6d4fd13ff4f9be76c121c6070368e6
slah ben kacem <slah.eddine.ben.kacem@gmail.com>
2024-03-12 14:28:19 +0100

    fix some work in home page and create new component for loading page with animation

--- front/my-app/functions/CheckToken.js
+++ front/my-app/functions/CheckToken.js
@@ -3,5 +3,5 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export default async function CheckToken () {
     const isToken = await AsyncStorage.getItem("token")
     console.log(isToken);
-    return (isToken)
+    return isToken
 }
